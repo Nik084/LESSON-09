65143674741d61a0e6ac25fe49069ebe2b674055
Nik084 <regul1200@gmail.com>
2024-11-01 23:54:59 +0300

    Update Modul-9_2.py

--- Modul-9_2.py
+++ Modul-9_2.py
@@ -4,10 +4,7 @@ second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler
 first_result = [len(x) for x in first_strings if len(x) >= 5]
 second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
 third_result = {x: len(x) for x in first_strings + second_strings if len(x) % 2 == False}
-fourth = [(first_strings[i], second_strings[i])
-          for i in range(min(len(first_strings), len(second_strings)))
-          if len(first_strings[i]) == len(second_strings[i])]
+
 print(first_result)
 print(second_result)
 print(third_result)
-print(fourth)
